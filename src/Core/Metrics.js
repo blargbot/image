e50811a8b4de7fa5458100188d3b5363d94e60f0
stupid cat <kirkbenm@gmail.com>
2018-05-10 11:47:23 -0600

    change metrics

--- src/Core/Metrics.js
+++ src/Core/Metrics.js
@@ -8,7 +8,7 @@ class Metrics {
             name: 'http_request_duration_ms',
             help: 'Duration of HTTP requests in MS',
             labelNames: ['route', 'successful'],
-            buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 4000, 5000]
+            buckets: [300, 400, 500, 750, 1000, 1500, 2000, 2500, 3000, 5000]
         });
 
         this.collectDefaultMetrics = Prometheus.collectDefaultMetrics;
